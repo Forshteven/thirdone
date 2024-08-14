a05ca137d0d892e17512e18eeec22ce3fcd0c851
Forshteven <geocinds@mail.ru>
2024-08-14 11:15:06 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ number_of_hours_spent = 1.5
 heading_name = 'Pyton'
 time_for_one_task = number_of_hours_spent/number_of_tasks
 print(time_for_one_task)
-print('Курс:',heading_name,', ''всего задач:',number_of_tasks,', ''затрачено часов:',number_of_hours_spent,', ''среднее время выполнения 0,125 часа.')
+print('Курс: ',heading_name,','' всего задач: ',number_of_tasks,',' ' затрачено часов: ',number_of_hours_spent,','' среднее время выполнения ',time_for_one_task,' часа.', sep='')
